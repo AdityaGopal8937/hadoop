c78e38612e97a705baf5d913454572bcffdd6307
AdityaGopal8937 <77403170+AdityaGopal8937@users.noreply.github.com>
2021-04-10 09:37:14 +0530

    Add files via upload

---
+++ addfloat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class addfloat
+{
+public static void main(String args[])
+{
+	float c,a,b;
+	a=Float.parseFloat(args[0]);
+	b=Float.parseFloat(args[1]);
+	c=a*b;
+	System.out.print(c);
+}
+}
